ab9e20ecf8f2b2b815957c19278ea391e457100d
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-13 23:50:35 +0000

    added url form submission page

--- expressServer.js
+++ expressServer.js
@@ -3,11 +3,18 @@ const app = express()
 const PORT = 8080
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("hello")
@@ -20,6 +27,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -36,10 +47,18 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
  
+
+
 //  app.get("/fetch", (req, res) => {
 //   res.send(`a = ${a}`);
 //  });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+function generateRandomString() {
+  let array = [];
+  for(let x = 0; x < 6; x++) {
+    array.push(Math.floor(Math.random() * 9).toString())
+  }
+  return array.join("")
+}
+
+
