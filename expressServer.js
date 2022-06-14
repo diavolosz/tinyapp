0f45a07f6f4e12213222c022ebce0fad2d87d1ff
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-14 19:58:26 +0000

    branch push test

--- expressServer.js
+++ expressServer.js
@@ -50,26 +50,14 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
-
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-//  });
- 
- app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
 
 
 
 
-
+//------------------------------------------------------------
 function generateRandomString() {
   let array = [];
   for(let x = 0; x < 6; x++) {
